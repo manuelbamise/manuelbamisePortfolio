2cff36ccca3d7c0cb6c53f849ced9c96804738d4
manuelbamise <etobi1930@gmail.com>
2025-07-07 15:09:36 +0100

    feat: the theme mode switching is morking correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-
+import { useEffect, useState } from "react";
+import { Button } from "./components/ui/button";
+import { useTheme } from "./components/use-theme"
+import type { Theme } from "./components/theme-provider";
 function App() {
-  return (
+
+  const [themeChoice, setThemeChoices] = useState<Theme>("dark");
+  const { setTheme } = useTheme()
+
+  useEffect(() => {
+    setTheme(themeChoice);
+    console.log(themeChoice);
+
+
+  }, [themeChoice, setTheme])
+
+  const toggleTheme = () => {
+    setThemeChoices((prev) => (prev === "dark" ? "light" : "dark"))
+  }
+  return (<>
+    <Button onClick={() => toggleTheme()}>
+      Button
+    </Button>
+
     <div className="bg-red-700 dark:bg-red-400">
       Hello from app.tsx
-    </div>
+    </div></>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
+import { ThemeProvider } from './components/theme-provider.tsx'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
