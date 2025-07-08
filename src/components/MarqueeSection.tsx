2743bc4efbddb96989b11682d23c5451909b1689
manuelbamise <etobi1930@gmail.com>
2025-07-08 12:51:29 +0100

    process: trying to work around the express svg

--- src/components/MarqueeSection.tsx
+++ src/components/MarqueeSection.tsx
@@ -1,5 +1,10 @@
 import Marquee from "react-fast-marquee";
 import reactSvg from "../assets/svg/react.svg"
+import nodeSvg from "../assets/svg/nodejs.svg"
+import typescriptSvg from "../assets/svg/typescript.svg"
+import expressSvg from "../assets/svg/express.svg"
+
+
 
 const MarqueeSection = () => {
 
@@ -8,6 +13,10 @@ const MarqueeSection = () => {
       <Marquee>
         <img src={reactSvg} alt="React svg" />
 
+        <img src={nodeSvg} alt="node svg" />
+        <img src={typescriptSvg} alt="typescript svg" />
+
+        <img src={expressSvg} alt="express svg" />
       </Marquee>
     </>
   )
