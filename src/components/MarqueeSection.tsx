ce22926beda28aecf6a8184c972e0d3a288fd5e2
manuelbamise <etobi1930@gmail.com>
2025-07-08 13:23:37 +0100

    process: added all the necessary svg's..now it's just to style the page properly

--- src/components/MarqueeSection.tsx
+++ src/components/MarqueeSection.tsx
@@ -3,20 +3,39 @@ import reactSvg from "../assets/svg/react.svg"
 import nodeSvg from "../assets/svg/nodejs.svg"
 import typescriptSvg from "../assets/svg/typescript.svg"
 import expressSvg from "../assets/svg/express.svg"
-
+import postgresSvg from "../assets/svg/postgres.svg"
+import neovimSvg from "../assets/svg/neovim.svg"
+import gitSvg from "../assets/svg/git.svg"
+import githubSvg from "../assets/svg/github.svg"
+import nestjsSvg from "../assets/svg/nestjs.svg"
+import tailwindSvg from "../assets/svg/tailwind.svg"
+import ubuntuSvg from "../assets/svg/ubuntu.svg"
+import vueSvg from "../assets/svg/vue.svg"
 
 
 const MarqueeSection = () => {
 
   return (
     <>
-      <Marquee>
-        <img src={reactSvg} alt="React svg" />
+      <Marquee className=" outline outline-black bg-white">
+        <div className="flex w-[1800px] items-center justify-between">
 
-        <img src={nodeSvg} alt="node svg" />
-        <img src={typescriptSvg} alt="typescript svg" />
+          <img src={reactSvg} alt="React svg" />
 
-        <img src={expressSvg} alt="express svg" />
+          <img src={nodeSvg} alt="node svg" />
+          <img src={typescriptSvg} alt="typescript svg" />
+
+          <img src={expressSvg} alt="express svg" />
+          <img src={postgresSvg} alt="postgres svg" />
+          <img src={neovimSvg} alt="postgres svg" />
+          <img src={gitSvg} alt="postgres svg" />
+          <img src={githubSvg} alt="postgres svg" />
+          <img src={nestjsSvg} alt="postgres svg" />
+          <img src={tailwindSvg} alt="postgres svg" />
+          <img src={ubuntuSvg} alt="postgres svg" />
+          <img src={vueSvg} alt="postgres svg" />
+
+        </div>
       </Marquee>
     </>
   )
