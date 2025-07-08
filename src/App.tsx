cc300253a7d20ab9d7a184187084583bb3e36969
manuelbamise <etobi1930@gmail.com>
2025-07-08 09:23:35 +0100

    process: the card with the placeholder image has been set on the page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Button } from "./components/ui/button";
 import { useTheme } from "./components/use-theme"
 import type { Theme } from "./components/theme-provider";
 import { Moon, Sun } from "lucide-react";
+import { Card } from "./components/ui/card";
 
 
 
@@ -40,16 +41,20 @@ function App() {
 
           </div>
 
-          <div className="mt-8 mx-5 flex justify-between">
-            <div className="w-[50%]">
-              <div>hi!! I;m hdsjebte</div>
+          <div className="mx-20 mt-8 flex items-center justify-around">
+            <div className="mt-4 w-[50%]">
+              <div className="text-5xl mb-2">hi!! I;m hdsjebte</div>
               <div>Creating innovative solutions and captivating designs.
                 University student by day, visionary creator by nighI'm a Software Engineer with 3+ years of experience building scalable, secure, and high-performance server-side applications using Node.js, Express.js, and Nest.js. I specialize in creating efficient APIs and microservices, and have contributed to various open-source projects. I'm passionate about making a positive impact and continuously expanding my skills in software engineering.</div>
             </div>
-            <div>images go here</div>
+            <div className=" w-[400px]">
+              <Card className="p-4 rounded-none w-full">
+                <img src="https://placehold.co/400" alt="placeholderImage" />
+              </Card>
+            </div>
           </div>
 
-          <div className="mt-13">
+          <div className="mt-13 mx-20">
             pMy coding journey kicked off when I was 9 years old. I began with block-based coding on websites like Code.org and Scratch. Eventually, I gained interest in web development and used online resources such as YouTube and Udemy to learn HTML, CSS, and JavaScript. A few years later, when I was 11, I got my first MacBook and wanted to learn iOS development, so I self-learned Swift and SwiftUI. Fast forward to when I was about to turn 13, I found out about the Swift Student Challenge and I started developing an offline app for it. When I submitted it, I thought I had a very slim chance of being one of the 350 winners, but I made it! I even got an offer from Apple to attend WWDC24 in-person. It was a very unique experience in Cupertino, meeting many new people. Right now, I am continuing to build more projects and learn new skills such as Next.js and Python.
           </div>
 
