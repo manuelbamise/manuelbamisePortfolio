00ea07d73b5aae70e79eaee581b8bfe085997b3f
manuelbamise <etobi1930@gmail.com>
2025-07-09 10:32:52 +0100

    bugFix: fixed the amount of unused space in the job experience dialog

--- src/components/ExperienceCards.tsx
+++ src/components/ExperienceCards.tsx
@@ -16,20 +16,20 @@ const ExperienceCards = () => {
         <Button variant="outline">see more</Button>
       </DialogTrigger>
 
-      <DialogContent className="sm:max-w-[425px] h-[50vh]"  >
-        {/* <DialogHeader className="outline outline-black"> */}
-        {/*   <DialogDescription>Jan 22 2024 - present</DialogDescription> */}
-        {/*   <DialogTitle>@Bimpe</DialogTitle> */}
-        {/* </DialogHeader> */}
-        <div className="outline outline-black">
-          <ul className="text-lg flex-col justify-between items-center"> <li> Architected and implemented scalable microservices using Node.js </li>
+      <DialogContent className="">
+        <DialogHeader className="outline outline-black mb-auto">
+          <DialogDescription>Jan 22 2024 - present</DialogDescription>
+          <DialogTitle>@Bimpe</DialogTitle>
+        </DialogHeader>
+        <div className="outline">
+          <ul className="text-lg outline outline-indigo-800">
+            <li className="mb-4"> Architected and implemented scalable microservices using Node.js </li>
 
-            <li>Mentored junior developers and conducted technical interviews </li>
+            <li className="mb-4">Mentored junior developers and conducted technical interviews </li>
 
-            <li>Collaborated with product managers to define technical specifications </li> </ul>
+            <li className="mb-4">Collaborated with product managers to define technical specifications </li> </ul>
         </div>
 
-        <div className="outline outline-black">this is something on the next line</div>
       </DialogContent>
     </Dialog>
   )
