8521489b03e882152ba6070179d0904b846ac152
manuelbamise <etobi1930@gmail.com>
2025-07-09 20:18:05 +0100

    process: routine save

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { GlassPillNav } from '../components/GlassPillNav'
 
@@ -6,15 +6,6 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <GlassPillNav />
-      {/* <div className="p-2 flex gap-2"> */}
-      {/*   <Link to="/" className="[&.active]:font-bold"> */}
-      {/*     Home */}
-      {/*   </Link>{' '} */}
-      {/*   <Link to="/about" className="[&.active]:font-bold"> */}
-      {/*     About */}
-      {/*   </Link> */}
-      {/* </div> */}
-
       <Outlet />
       <TanStackRouterDevtools />
     </>
