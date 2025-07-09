4b5985ef1ab6d0cbb640dd086c5091abe5558255
manuelbamise <etobi1930@gmail.com>
2025-07-09 10:35:37 +0100

    process: routine save

--- src/components/ExperienceCards.tsx
+++ src/components/ExperienceCards.tsx
@@ -13,22 +13,22 @@ const ExperienceCards = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline">see more</Button>
+        <Button variant="link">see more</Button>
       </DialogTrigger>
 
-      <DialogContent className="">
-        <DialogHeader className="outline outline-black mb-auto">
+      <DialogContent >
+        <DialogHeader className="mb-auto">
           <DialogDescription>Jan 22 2024 - present</DialogDescription>
           <DialogTitle>@Bimpe</DialogTitle>
         </DialogHeader>
-        <div className="outline">
-          <ul className="text-lg outline outline-indigo-800">
-            <li className="mb-4"> Architected and implemented scalable microservices using Node.js </li>
 
-            <li className="mb-4">Mentored junior developers and conducted technical interviews </li>
+        <ul className="text-lg ">
+          <li className="mb-4"> Architected and implemented scalable microservices using Node.js </li>
 
-            <li className="mb-4">Collaborated with product managers to define technical specifications </li> </ul>
-        </div>
+          <li className="mb-4">Mentored junior developers and conducted technical interviews </li>
+
+          <li className="mb-4">Collaborated with product managers to define technical specifications </li> </ul>
+
 
       </DialogContent>
     </Dialog>
