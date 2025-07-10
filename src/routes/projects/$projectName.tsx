9bce765596b6dad6fdc16350c896e6cfc6e51749
manuelbamise <etobi1930@gmail.com>
2025-07-10 15:36:01 +0100

    process: routine save

--- src/routes/projects/$projectName.tsx
+++ src/routes/projects/$projectName.tsx
@@ -1,25 +1,43 @@
-import { createFileRoute } from '@tanstack/react-router'
+
+import { createFileRoute, Link } from '@tanstack/react-router'
+import projects from '../../lib/json/projects.json'
+import { Button } from '../../components/ui/button';
+import { ArrowBigLeft } from 'lucide-react';
 
 export const Route = createFileRoute('/projects/$projectName')({
+  loader: ({ params }) => {
+    const project = projects.find(
+      (p) => p.projectName.toLowerCase() === params.projectName.toLowerCase()
+    );
+
+    if (!project) {
+      throw new Error('Project not found');
+    }
+
+    return project;
+  },
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
+  const project = Route.useLoaderData();
 
+  return (
+    <>
+      <div className=''>
 
+        <Button variant="secondary" size="lg" asChild>
 
+          <Link to='/projects'> <ArrowBigLeft /> </Link>
 
+        </Button>
+      </div>
+      <div className="p-6 space-y-4 mt-13">
+        <h1 className="text-3xl font-bold">{project.projectName}</h1>
+        <p className="text-gray-700">{project.longerText}</p>
+      </div>
 
-  const { projectName } = Route.useParams()
-  return <>
-
-
-    <div>
-
-
-      {projectName}
-    </div>
-
-
-  </>
+    </>
+  );
 }
+
