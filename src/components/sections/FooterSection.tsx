b46ea0258c27bb0778c06f2b67232c8545e87a5c
manuelbamise <etobi1930@gmail.com>
2025-07-11 14:36:31 +0100

    process: routine save

--- src/components/sections/FooterSection.tsx
+++ src/components/sections/FooterSection.tsx
@@ -1,11 +1,12 @@
+import { FaCross } from "react-icons/fa";
 
 
 const Footer = () => {
   return (
 
-    <div className="flex items-center justify-center mt-13">
-      <div>Footer</div>
-      <div>built my manuelbamise...links </div>
+    <div className="flex items-center justify-center mt-13 p-6 gap-4">
+      built with faith <FaCross /> by <a href="https://github.com/manuelbamise" className="hover:text-blue-700">bamise</a>
+
     </div>
   )
 }
