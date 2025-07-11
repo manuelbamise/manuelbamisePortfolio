8ec9bb90b53fcecfecd214244f5eac572d7d0daf
manuelbamise <etobi1930@gmail.com>
2025-07-11 12:47:42 +0100

    process: routine save

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -1,36 +1,39 @@
+import { FaGithub, FaLinkedin, FaTelegram, FaYoutube } from "react-icons/fa"
 import { Button } from "../ui/button"
-import { Github, Mail, PlaneTakeoff, Twitter, Youtube } from "lucide-react"
+import { FaXTwitter } from "react-icons/fa6"
 
 
 const ContectSection = () => {
+
+
+  const links = [
+    { key: 1, href: "https://x.com/@manuelbamise", logo: <FaXTwitter className="text-6xl" /> },
+    { key: 2, href: "#", logo: <FaLinkedin /> },
+    { key: 3, href: "https://youtube.com/@manuelbamise1", logo: <FaYoutube /> },
+    { key: 4, href: "#", logo: <FaTelegram /> },
+    { key: 5, href: "https://github.com/manuelbamise", logo: <FaGithub /> },
+  ]
+
+
+
+
   return (
     <>
+
       <div className="flex-col items-center justify-center ">
         <div className="flex-col items-center justify-around text-center ">
           <div className="text-3xl">contact </div>
           <div>here's how you can reach out to me</div>
         </div>
-        <div className="flex items-center justify-center gap-10 flex-wrap mt-4">
-          <Button asChild>
-            <a href="#"><Mail /> </a>
-          </Button>
+        <div className="flex items-center justify-center gap-10 flex-wrap mt-4 outline">
 
-          <Button asChild>
-            <a href="#"><Github /> </a>
-          </Button>
+          {links && links.map((link) => (
 
-          <Button asChild>
-            <a href="#"><Twitter /> </a>
-          </Button>
+            <Button asChild key={link.key} className="w-[10vw] p-6">
+              <a target="_blank" href={link.href} >{link.logo}</a>
+            </Button>
 
-
-          <Button asChild>
-            <a href="#"><Youtube /> </a>
-          </Button>
-
-          <Button asChild>
-            <a href="#"><PlaneTakeoff /> </a>
-          </Button>
+          ))}
 
         </div>
       </div>
--- src/routes/blog.tsx
+++ src/routes/blog.tsx
@@ -1,8 +1,39 @@
 import { createFileRoute } from '@tanstack/react-router'
-import ComingSoonComponent from '../components/AdditionalComponents/ComingSoon'
+import Footer from '../components/sections/FooterSection'
+// import ComingSoonComponent from '../components/AdditionalComponents/ComingSoon'
 
 
 export const Route = createFileRoute('/blog')({
-  component: ComingSoonComponent,
+  component: RouteComponent,
 })
 
+
+function RouteComponent() {
+
+  const scriptLink = "https://js.supascribe.com/v1/loader/zvIyWyAxE5Yq6S2qjKR7gMN0TG52.js"
+
+
+  return (
+
+    <>
+
+      <div className='mx-20 flex-col '>
+
+        <div>
+          {/* <!-- Feed embed code --> */}
+          <div data-supascribe-embed-id="396016599949" data-supascribe-feed></div>
+
+        </div>
+
+        <div className='text-center'>
+          <Footer />
+        </div>
+      </div>
+
+      {/* <!-- Supascribe Script (Only add this to your site once) --> */}
+      <script src={scriptLink} async></script>
+
+    </>
+
+  )
+}
