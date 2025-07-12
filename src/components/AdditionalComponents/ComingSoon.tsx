5cabd21493b92058ba706d146314bb7ae7b098cf
manuelbamise <etobi1930@gmail.com>
2025-07-12 07:23:34 +0100

    process: made a change to the coming soon Component, i don't really know what else to add there

--- src/components/AdditionalComponents/ComingSoon.tsx
+++ src/components/AdditionalComponents/ComingSoon.tsx
@@ -3,18 +3,14 @@
 const ComingSoonComponent = () => {
   return (
     <>
-
       <div className="flex items-center justify-center mt-13 ">
-
         <div className="w-[25%] flex-col text-center dark:border dark:border-white rounded-lg shadow-xl p-6">
 
           <div className="text-5xl font-bold">Coming Soon</div>
-          <div className="text-gray-700 dark:text-white mt-6">This lorwr cus nf sdasnd asdj liahfkhco kadkn enihbcnn zhonnksod nd knshabda djb djkjasdkn  jndkja dm  djbadd.</div>
-
+          <div className="text-gray-700 dark:text-white mt-6">This page is still under construction.</div>
 
         </div>
       </div>
-
     </>
   )
 }
