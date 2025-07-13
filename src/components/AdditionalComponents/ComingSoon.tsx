2718f0495334598061f12dbaac8cfffd08b4d751
manuelbamise <etobi1930@gmail.com>
2025-07-13 21:31:25 +0100

    process: routine save

--- src/components/AdditionalComponents/ComingSoon.tsx
+++ src/components/AdditionalComponents/ComingSoon.tsx
@@ -4,7 +4,7 @@ const ComingSoonComponent = () => {
   return (
     <>
       <div className="flex items-center justify-center mt-13 ">
-        <div className="w-[25%] flex-col text-center dark:border dark:border-white rounded-lg shadow-xl p-6">
+        <div className=" flex-col text-center dark:border dark:border-white rounded-lg shadow-xl p-6">
 
           <div className="text-5xl font-bold">Coming Soon</div>
           <div className="text-gray-700 dark:text-white mt-6">This page is still under construction.</div>
