dabb72419049e225c4230be650cf3783085e99e6
manuelbamise <etobi1930@gmail.com>
2025-07-14 14:12:49 +0100

    process: routine save

--- src/components/sections/MarqueeSection.tsx
+++ src/components/sections/MarqueeSection.tsx
@@ -30,7 +30,7 @@ const MarqueeSection = () => {
     <>
       <div className="w-full py-6">
         <Marquee pauseOnHover={true} gradient={true} gradientWidth={50} >
-          <div className="flex items-center gap-8 justify-around px-4 outline lg:w-[100vw] w-full">
+          <div className="flex items-center gap-8 justify-around px-4 lg:w-[100vw] w-full">
             {techStack.map((icon, index) => (
               <img
                 key={index}
