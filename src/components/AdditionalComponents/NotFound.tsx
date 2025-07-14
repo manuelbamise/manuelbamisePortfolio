e8a777c60292b7121332f991832431e4588c45c4
manuelbamise <etobi1930@gmail.com>
2025-07-14 10:53:31 +0100

    process: routine save

--- src/components/AdditionalComponents/NotFound.tsx
+++ src/components/AdditionalComponents/NotFound.tsx
@@ -7,9 +7,15 @@ const NotFound = () => {
       <div className="flex items-center justify-center mt-13 ">
         <div className=" flex-col text-center dark:border dark:border-white rounded-lg shadow-xl p-6">
 
-          <div className="text-5xl font-bold">Not Found</div>
-          <div className="text-gray-700 dark:text-white mt-6">This page is unavailable.. check the url or use the navigation to proceed to home</div>
+          <div className="text-5xl font-bold">
+            404 – Page Not Found
+          </div>
+          <div className="text-gray-700 dark:text-white mt-6">Oops! The page you're looking for doesn't exist.</div>
 
+          <div className=" mt-2">
+            <div> Try checking the address again or head back to the homepage.</div>
+
+          </div>
         </div>
       </div>
     </>
