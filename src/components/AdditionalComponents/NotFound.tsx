887d0b8f9209733a8d15965a45c20fe067d950eb
manuelbamise <etobi1930@gmail.com>
2025-07-15 14:14:24 +0100

    process: routine save

--- src/components/AdditionalComponents/NotFound.tsx
+++ src/components/AdditionalComponents/NotFound.tsx
@@ -1,10 +1,15 @@
+import { motion } from "framer-motion";
 
 const NotFound = () => {
 
   return (
 
     <>
-      <div className="flex items-center justify-center mt-13 ">
+      <motion.div className="flex items-center justify-center mt-13 "
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, ease: 'easeOut' }}
+      >
         <div className=" flex-col text-center dark:border dark:border-white rounded-lg shadow-xl p-6">
 
           <div className="text-5xl font-bold">
@@ -17,7 +22,7 @@ const NotFound = () => {
 
           </div>
         </div>
-      </div>
+      </motion.div>
     </>
   )
 }
