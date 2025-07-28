87f8c43a30708618c508484b17bae65b3a819254
manuelbamise <etobi1930@gmail.com>
2025-07-28 12:27:09 +0100

    process: routine save

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
   BookOpen,
   Linkedin
 } from 'lucide-react'
+import ThemeSwitcher from './lib/theme-config/themeSwitcher'
 
 const socialIcons = [
   { icon: <Home className="w-5 h-5" />, href: '#' },
@@ -27,6 +28,9 @@ const experiences = [
 export default function App() {
   return (
     <div className="max-w-3xl mx-auto p-4 font-sans">
+
+      <ThemeSwitcher />
+
       {/* Top Nav */}
       <div className="flex items-center justify-between border rounded-full px-4 py-2 shadow-sm mb-6">
         <div className="flex space-x-4">
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.tsx'
+import { ThemeProvider } from './lib/theme-config/themeProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme='system' storageKey='viteTheme'>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
